dc551ae12e0957fcd5e00bd08db9eb142019ad40
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-21 10:59:54 +0530

    Email code

--- EmailSend/Program.cs
+++ EmailSend/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace EmailSend
 {
@@ -6,9 +8,30 @@ namespace EmailSend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rn = new Random();
+            int otp = rn.Next(1000);
+            string OTP = otp.ToString();
 
-           // change
+
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com"); 
+
+            mail.From = new MailAddress("vishnukumarps2017@gmail.com"); // From
+
+            mail.To.Add("vishnuvis0016@gmail.com");// To
+            mail.Subject = "OTP ";
+            mail.Body = "YOUR OTP="+ OTP;
+
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("vishnukumarps2017@gmail.com", "vISHNU@123");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+
+            Console.WriteLine("Send"+OTP);
+            Console.ReadLine();
         }
     }
 }
