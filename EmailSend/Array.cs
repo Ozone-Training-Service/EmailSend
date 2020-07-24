5e3534f21ad891ea7e92de85c3823ff1140dac65
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-24 10:25:17 +0530

    2D Array

--- EmailSend/Array.cs
+++ EmailSend/Array.cs
@@ -4,43 +4,73 @@ using System.Text;
 
 namespace EmailSend
 {
+
+    
     class Array
     {
 
         static void Main(string[] args)
         {
-            int[] a = new int[10];
+            //int[] a = new int[10];
 
-            a[0] = 10;
-            a[9] = 22;
+            //a[0] = 10;
+            //a[9] = 22;
 
-            Console.WriteLine("Enter Array Elments");
-            for (int i = 0; i < a.Length; i++)
+            //Console.WriteLine("Enter Array Elments");
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    a[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+
+            //Console.WriteLine("output");
+
+            ////            9>=0
+
+            //for (int i=0;i<=5;i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+            //// foreach
+            //// array print
+            ////Collection
+            ////speed
+
+            //Console.WriteLine("output");
+
+            //foreach (var item in a)
+            //{
+
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            int[,] b = new int[2, 2];
+            b[0, 0] = 10;
+
+            Console.WriteLine("Enter a 2X2 matrix");
+            for (int i = 0; i < 2; i++)
             {
-                a[i] = Convert.ToInt32(Console.ReadLine());
+                for (int j = 0; j < 2; j++)
+                {
+                    b[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
             }
+            Console.WriteLine("Tanspose");
 
-
-            Console.WriteLine("output");
-
-            //            9>=0
-
-            for (int i=0;i<=5;i++)
+            for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine(a[i]);
-            }
+                for (int j = 0; j < 2; j++)
+                {
 
-            // foreach
-            // array print
-            //Collection
-            //speed
+                    Console.Write(b[j, i] + "  ");// transpose
+                    
+                    //Console.Write(b[i,j]+"  ");
+                }
 
-            Console.WriteLine("output");
-
-            foreach (var item in a)
-            {
-               
-                Console.WriteLine(item);
+                Console.WriteLine("\n");
             }
 
 
