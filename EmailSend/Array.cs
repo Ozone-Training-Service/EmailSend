2f1f34e5dc4d899871f5b5c5319938075dbbbf76
aji vp <ajivpvperavu@gmail.com>
2020-07-24 12:23:26 +0530

    ar

--- EmailSend/Array.cs
+++ EmailSend/Array.cs
@@ -2,86 +2,90 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EmailSend
+namespace EmailSend.Version1
 {
 
+
     
-    class Array
-    {
+        //int[] a = new int[10];
 
-        static void Main(string[] args)
+        //a[0] = 10;
+        //a[9] = 22;
+
+        //Console.WriteLine("Enter Array Elments");
+        //for (int i = 0; i < a.Length; i++)
+        //{
+        //    a[i] = Convert.ToInt32(Console.ReadLine());
+        //}
+
+
+        //Console.WriteLine("output");
+
+        ////            9>=0
+
+        //for (int i=0;i<=5;i++)
+        //{
+        //    Console.WriteLine(a[i]);
+        //}
+
+        //// foreach
+        //// array print
+        ////Collection
+        ////speed
+
+        //Console.WriteLine("output");
+
+        //foreach (var item in a)
+        //{
+
+        //    Console.WriteLine(item);
+        //}
+        class Array
         {
-            //int[] a = new int[10];
 
-            //a[0] = 10;
-            //a[9] = 22;
-
-            //Console.WriteLine("Enter Array Elments");
-            //for (int i = 0; i < a.Length; i++)
-            //{
-            //    a[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-
-
-            //Console.WriteLine("output");
-
-            ////            9>=0
-
-            //for (int i=0;i<=5;i++)
-            //{
-            //    Console.WriteLine(a[i]);
-            //}
-
-            //// foreach
-            //// array print
-            ////Collection
-            ////speed
-
-            //Console.WriteLine("output");
-
-            //foreach (var item in a)
-            //{
-
-            //    Console.WriteLine(item);
-            //}
-
-
-
-            int[,] b = new int[2, 2];
-            b[0, 0] = 10;
-
-            Console.WriteLine("Enter a 2X2 matrix");
-            for (int i = 0; i < 2; i++)
+            static void Main(string[] args)
             {
-                for (int j = 0; j < 2; j++)
+
+
+                int[,] b = new int[2, 2];
+                b[0, 0] = 10;
+
+                Console.WriteLine("Enter a 2X2 matrix");
+                for (int i = 0; i < 2; i++)
                 {
-                    b[i, j] = Convert.ToInt32(Console.ReadLine());
+                    for (int j = 0; j < 2; j++)
+                    {
+                        b[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
                 }
-            }
-            Console.WriteLine("Tanspose");
+                Console.WriteLine("Tanspose");
 
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < 2; j++)
+                for (int i = 0; i < 2; i++)
                 {
+                    for (int j = 0; j < 2; j++)
+                    {
 
-                    Console.Write(b[j, i] + "  ");// transpose
-                    
-                    //Console.Write(b[i,j]+"  ");
+                        Console.Write(b[j, i] + "  ");// transpose
+
+                        //Console.Write(b[i,j]+"  ");
+                    }
+
+                    Console.WriteLine("\n");
                 }
 
-                Console.WriteLine("\n");
+
+
+
+
+
+
+
+
+                Console.ReadKey();
             }
-
-
-
-
-
-
-
-
-
-            Console.ReadKey();
         }
     }
-}
+
+
+
+
---
+++ EmailSend/Version1/arraysearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailSend.Version1
+{
+    class arraysearch
+    {
+        static void Main(string[] args)
+            {
+            int n;
+                int[] a = new int[10];
+                Console.WriteLine("enter array elements");
+                for (int i = 0; i < a.Length; i++)
+                {
+
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("enter search element");
+                n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < a.Length; i++)
+                {
+                if (n == a[i])
+                {
+                    Console.WriteLine("element found");
+                    break;
+
+
+                }
+                else
+                {
+                    
+                }
+
+                }
+           
+            }
+        
+        }
+}
